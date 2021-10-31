3a3e723f475af592ebed7c4bba3f7af9d719b5ab
David <kogand2@mcmaster.ca>
2021-10-31 16:44:23 -0400

    final push

--- dcm_layouts.py
+++ dcm_layouts.py
@@ -1,54 +1,73 @@
 # Imports
 import PySimpleGUI as sg
 
+
 # Set theme of GUI
 sg.theme('Dark')
 
-# Set-up the layouts of each pacing mode tab
-AOO_tab = [  [sg.Text('Pacing Mode - AOO', font=("Helvetica", 20))],
-             [sg.Graph((500, 250), (0,0), (500, 250), background_color='white', key="-GRAPH-")],
-             [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
-             [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText('1', key='AOO-LRL')],
-             [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText('1', key='AOO-URL')],
-             [sg.Text('Atrial Amplitude', size=(16,1)), sg.InputText('1', key='AOO-AA')],
-             [sg.Text('Atrial Pulse Width', size=(16,1)), sg.InputText('1', key='AOO-APW')],
-             [sg.Button('Submit')] ]
+#method so that the pacemaker screens can access data
+def make_parameters(data):
+    AOO_layout = [  [sg.Text('Pacing Mode - AOO', font=("Helvetica", 20))],
+                    [sg.Graph((500, 250), (0,0), (500, 250), background_color='white', key="-GRAPH-")],
+                    [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
+                    [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText(data[0], key='AOO-LRL')],
+                    [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText(data[1], key='AOO-URL')],
+                    [sg.Text('Atrial Amplitude', size=(16,1)), sg.InputText(data[2], key='AOO-AA')],
+                    [sg.Text('Atrial Pulse Width', size=(16,1)), sg.InputText(data[3], key='AOO-APW')],
+                    [sg.Button('Submit', key='SUB-AOO')] ]
 
-VOO_tab = [  [sg.Text('Pacing Mode - VOO', font=("Helvetica", 20))],
-             [sg.Graph((500, 250), (0,0), (500, 250), background_color='white')],
-             [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
-             [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText('1', key='VOO-LRL')],
-             [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText('1', key='VOO-URL')],
-             [sg.Text('Ventrical Amplitude', size=(16,1)), sg.InputText('1', key='VOO-VA')],
-             [sg.Text('Ventrical Pulse Width', size=(16,1)), sg.InputText('1', key='VOO-VPW')],
-             [sg.Button('Submit')] ]
+    VOO_layout = [  [sg.Text('Pacing Mode - VOO', font=("Helvetica", 20))],
+                    [sg.Graph((500, 250), (0,0), (500, 250), background_color='white')],
+                    [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
+                    [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText(data[4], key='VOO-LRL')],
+                    [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText(data[5], key='VOO-URL')],
+                    [sg.Text('Ventrical Amplitude', size=(16,1)), sg.InputText(data[6], key='VOO-VA')],
+                    [sg.Text('Ventrical Pulse Width', size=(16,1)), sg.InputText(data[7], key='VOO-VPW')],
+                    [sg.Button('Submit', key='SUB-VOO')] ]
 
-AAI_tab = [  [sg.Text('Pacing Mode - AAI', font=("Helvetica", 20))],
-             [sg.Graph((500, 250), (0,0), (500, 250), background_color='white')],
-             [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
-             [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText('1', key='AAI-LRL')],
-             [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText('1', key='AAI-URL')],
-             [sg.Text('Atrial Amplitude', size=(16,1)), sg.InputText('1', key='AAI-AA')],
-             [sg.Text('Atrial Pulse Width', size=(16,1)), sg.InputText('1', key='AAI-APW')],
-             [sg.Text('Atrial Sensitivity', size=(16,1)), sg.InputText('1', key='AAI-AS')],
-             [sg.Text('ARP', size=(16,1)), sg.InputText('1', key='AAI-ARP')],
-             [sg.Text('PVARP', size=(16,1)), sg.InputText('1', key='AAI-PVARP')],
-             [sg.Text('Hysteresis', size=(16,1)), sg.InputText('1', key='AAI-H')],
-             [sg.Text('Rate Smoothing', size=(16,1)), sg.InputText('1', key='AAI-RS')],
-             [sg.Button('Submit')] ]
+    AAI_layout = [  [sg.Text('Pacing Mode - AAI', font=("Helvetica", 20))],
+                    [sg.Graph((500, 250), (0,0), (500, 250), background_color='white')],
+                    [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
+                    [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText(data[8], key='AAI-LRL')],
+                    [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText(data[9], key='AAI-URL')],
+                    [sg.Text('Atrial Amplitude', size=(16,1)), sg.InputText(data[10], key='AAI-AA')],
+                    [sg.Text('Atrial Pulse Width', size=(16,1)), sg.InputText(data[11], key='AAI-APW')],
+                    [sg.Text('Atrial Sensitivity', size=(16,1)), sg.InputText(data[12], key='AAI-AS')],
+                    [sg.Text('ARP', size=(16,1)), sg.InputText(data[13], key='AAI-ARP')],
+                    [sg.Text('PVARP', size=(16,1)), sg.InputText(data[14], key='AAI-PVARP')],
+                    [sg.Text('Hysteresis', size=(16,1)), sg.InputText(data[15], key='AAI-H')],
+                    [sg.Text('Rate Smoothing', size=(16,1)), sg.InputText(data[16], key='AAI-RS')],
+                    [sg.Button('Submit', key='SUB-AAI')] ]
 
-VVI_tab = [  [sg.Text('Pacing Mode - VVI', font=("Helvetica", 20))],
-             [sg.Graph((500, 250), (0,0), (500, 250), background_color='white')],
-             [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
-             [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText('1', key='VVI-LRL')],
-             [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText('1', key='VVI-URL')],
-             [sg.Text('Ventrical Amplitude', size=(16,1)), sg.InputText('1', key='VVI-VA')],
-             [sg.Text('Ventrical Pulse Width', size=(16,1)), sg.InputText('1', key='VVI-VPW')],
-             [sg.Text('Ventrical Sensitivity', size=(16,1)), sg.InputText('1', key='VVI-VS')],
-             [sg.Text('VRP', size=(16,1)), sg.InputText('1', key='VVI-VRP')],
-             [sg.Text('Hysteresis', size=(16,1)), sg.InputText('1', key='VVI-H')],
-             [sg.Text('Rate Smoothing', size=(16,1)), sg.InputText('1', key='VVI-RS')],
-             [sg.Button('Submit')] ]
+    VVI_layout = [  [sg.Text('Pacing Mode - VVI', font=("Helvetica", 20))],
+                    [sg.Graph((500, 250), (0,0), (500, 250), background_color='white')],
+                    [sg.Text('Enter Parameters:', font=("Helvetica", 16))],
+                    [sg.Text('Lower Rate Limit', size=(16,1)), sg.InputText(data[17], key='VVI-LRL')],
+                    [sg.Text('Upper Rate Limit', size=(16,1)), sg.InputText(data[18], key='VVI-URL')],
+                    [sg.Text('Ventrical Amplitude', size=(16,1)), sg.InputText(data[19], key='VVI-VA')],
+                    [sg.Text('Ventrical Pulse Width', size=(16,1)), sg.InputText(data[20], key='VVI-VPW')],
+                    [sg.Text('Ventrical Sensitivity', size=(16,1)), sg.InputText(data[21], key='VVI-VS')],
+                    [sg.Text('VRP', size=(16,1)), sg.InputText(data[22], key='VVI-VRP')],
+                    [sg.Text('Hysteresis', size=(16,1)), sg.InputText(data[23], key='VVI-H')],
+                    [sg.Text('Rate Smoothing', size=(16,1)), sg.InputText(data[24], key='VVI-RS')],
+                    [sg.Button('Submit', key='SUB-VVI')] ]
+
+    pacing_layout = [ 
+        [
+            sg.TabGroup
+            (
+                [
+                    [
+                        sg.Tab('AOO', AOO_layout),
+                        sg.Tab('VOO', VOO_layout),
+                        sg.Tab('AAI', AAI_layout), 
+                        sg.Tab('VVI', VVI_layout)
+                    ]
+                ]
+            )
+        ]
+    ]
+    return pacing_layout
 
 # Set up the columns for the welcome/login screen
 welcome_column = [
@@ -72,22 +91,5 @@ landing_layout = [
         sg.Column(welcome_column),
         sg.VSeperator(),
         sg.Column(log_in_column),
-    ]
-]
-
-# Setup the layout for the pacing screen 
-pacing_layout = [ 
-    [
-        sg.TabGroup
-        (
-            [
-                [
-                    sg.Tab('AOO', AOO_tab),
-                    sg.Tab('VOO', VOO_tab),
-                    sg.Tab('AAI', AAI_tab), 
-                    sg.Tab('VVI', VVI_tab)
-                ]
-            ]
-        )
     ]
 ]
--- pacemaker_dcm.py
+++ pacemaker_dcm.py
@@ -1,6 +1,7 @@
 # Imports
 import PySimpleGUI as sg
-from dcm_layouts import landing_layout, pacing_layout
+from dcm_layoutsv2 import landing_layout, make_parameters
+import json
 
 # Set theme of GUI
 sg.theme('Dark')
@@ -11,15 +12,14 @@ class DCM:
         # Initialize landing window layout and run program
         self.window = sg.Window('Pacemaker DCM', landing_layout)
         self.run_welcome_screen()
-
+        
     def run_welcome_screen(self):
-
         # Infinite loop so that the program stays running until the window is closed
+        with open('parameter_limits.json') as f:
+            param_limits = json.load(f)
         while True:
-
             # Read in event and input values
             event, values = self.window.read()
-
             # Termination
             if event == "Exit" or event == sg.WIN_CLOSED:
                 break
@@ -27,16 +27,17 @@ class DCM:
             # Login process
             if event == "Login":
                 if DCM.verify(values["-name-"], values["-password-"]):
-
+                    self.username = values["-name-"]
                     # If login is successful, proceed to pacing screens
                     self.window.close()
                     self.run_pacing_screen()
+
                 else:
                     sg.popup("User doesn't exist. Please register.")
 
             # Registration process
             elif event == "Register":
-
+                self.username = values["-name-"]
                 # Ensure the fields are not empty
                 if not values["-name-"] and not values["-password-"]:
                     sg.popup("Credentials cannot be empty.")
@@ -45,17 +46,27 @@ class DCM:
                 elif DCM.existing_name(values["-name-"]):
                     sg.popup("User already exists.")
 
+                # Max 10 Users
+                elif DCM.is_db_full():
+                    sg.popup("Max user capacity reached.")
+
                 # Successful registration
                 else:
                     DCM.register(values["-name-"], values["-password-"])
+                    DCM.default_paramaters(self.username, param_limits)
                     sg.popup("User successfully registered.")
+                
+            if event in ['SUB-AOO', 'SUB-VOO', 'SUB-AAI', 'SUB-VVI']:
+                DCM.submit_parameters(self.username, values, param_limits)
+
 
         self.window.close()
 
     def run_pacing_screen(self):
 
         # Initialize pacing window layout
-        self.window = sg.Window('Pacemaker DCM', pacing_layout)
+        pace_lay = make_parameters(DCM.set_paramaters(self.username))
+        self.window = sg.Window('Pacemaker DCM', pace_lay)
 
     # This method authenticates the user login
     @staticmethod
@@ -75,6 +86,13 @@ class DCM:
                 return True
         return False
 
+    # This method checks if db at full capacity
+    @staticmethod
+    def is_db_full():
+        if sum(1 for line in open('user_data.txt')) == 10:
+            return True
+        return False
+
     # This method registers new users by writing it to the database(txt file)
     @staticmethod
     def register(name, password):
@@ -85,5 +103,83 @@ class DCM:
         file.write("\n")
         file.close()
 
+    #This method is for giving a new registered user default parameters
+    @staticmethod
+    def default_paramaters(name, param_limits):
+        count=0
+        file = open("user_data.txt", "r")
+        list_of_lines = file.readlines()
+
+        for line in list_of_lines:
+            user = line.split()
+            if name == user[0]:
+                file.close()
+                break
+            count +=1
+
+        default_params = ''
+        for param in param_limits:
+            default_params += str(param_limits[param]['low']) + ' '
+        default_params += '\n'
+
+        list_of_lines[count]= list_of_lines[count].replace("\n"," ") + default_params
+
+        a_file = open("user_data.txt", "w")
+        a_file.writelines(list_of_lines)
+        a_file.close()
+
+    #This method sends the inputed values to the text file with all the data
+    @staticmethod
+    def submit_parameters(name, values, param_limits):
+        invalid_value_flag = False
+        count = 0
+        list_of_keys =['AOO-LRL','AOO-URL','AOO-AA','AOO-APW','VOO-LRL','VOO-URL','VOO-VA','VOO-VPW','AAI-LRL','AAI-URL','AAI-AA','AAI-APW','AAI-AS','AAI-ARP','AAI-PVARP','AAI-H','AAI-RS','VVI-LRL','VVI-URL','VVI-VA','VVI-VPW','VVI-VS','VVI-VRP','VVI-H','VVI-RS']
+        a_file = open("user_data.txt", "r")
+        list_of_lines = a_file.readlines()
+
+        for line in list_of_lines:
+            user = line.split()
+            if name == user[0]:
+                a_file.close()
+                break
+            count +=1
+        user = list_of_lines[count].split()
+        for x in range(len(user)):
+            if(x==0 or x==1):
+                continue
+            curr_key = list_of_keys[x-2]
+
+            if float(values[curr_key]) >= param_limits[curr_key]['low'] and float(values[curr_key]) <= param_limits[curr_key]['high']:
+                user[x] = values[curr_key]
+            else:
+                sg.popup("Parameter out of bounds: {}".format(curr_key))
+                invalid_value_flag = True
+
+        if not invalid_value_flag:
+            list_of_lines[count]= " ".join(user) + "\n"
+            a_file = open("user_data.txt", "w")
+            a_file.writelines(list_of_lines)
+            a_file.close()
+
+    #This method is used for gathering the saved values the user had submitted previously
+    @staticmethod
+    def set_paramaters(name):
+        count = 0
+        a_file = open("user_data.txt", "r")
+        list_of_lines = a_file.readlines()
+        for line in list_of_lines:
+            user = line.split()
+            if name == user[0]:
+                a_file.close()
+                break
+            count +=1
+        data = []
+        user = list_of_lines[count].split()
+        for x in range(len(user)):
+            if(x==0 or x==1):
+                continue
+            data.append(user[x])
+        return data
+            
 # Run module
 program = DCM()
