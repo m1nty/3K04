bd3c2466c4670b2ba3bd6926aba0974b619304fa
Minhaj Shah <shahm23@mcmaster.ca>
2021-11-24 15:32:23 -0500

    tweak

--- port_list.py
+++ port_list.py
@@ -2,4 +2,9 @@ import serial.tools.list_ports
 
 ports = serial.tools.list_ports.comports()
 
-print([port.name for port in ports])
+available_ports = []
+
+for p in ports:
+    available_ports.append(p.device)
+
+print(available_ports)
