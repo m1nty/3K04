4be2618e5b9ee70afbeb0945fbc4a745e99793c4
Minhaj Shah <shahm23@mcmaster.ca>
2021-11-24 15:30:13 -0500

    port lister tweak

--- port_list.py
+++ port_list.py
@@ -1,5 +1,5 @@
 import serial.tools.list_ports
+
 ports = serial.tools.list_ports.comports()
 
-for port, desc, hwid in sorted(ports):
-        print("{}: {} [{}]".format(port, desc, hwid))
+print([port.name for port in ports])
