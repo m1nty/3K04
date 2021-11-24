167bcd5f139e156fc2a52f219447679280368c27
Minhaj Shah <shahm23@mcmaster.ca>
2021-11-24 15:40:02 -0500

    func tweak

--- port_list.py
+++ port_list.py
@@ -1,10 +1,35 @@
-import serial.tools.list_ports
+import sys
+import glob
+import serial
 
-ports = serial.tools.list_ports.comports()
 
-available_ports = []
+def serial_ports():
+    """ Lists serial port names
 
-for p in ports:
-    available_ports.append(p.device)
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
-print(available_ports)
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+print(serial_ports())
